3af43ed80ea7457c7eaf319d98faef09a6192254
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-11-14 00:48:31 +0330

    add

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+    import { ModeWatcher } from 'mode-watcher';
+    import Nav from './components/Navbar/nav.svelte';
+
+	
+
+
+
+
+
+
+	
+	let { children } = $props();
+	
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+<ModeWatcher />
+
+
+
+
+<Nav />
+{@render children()}
---
+++ src/routes/professors/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import * as Table from "$lib/components/ui/table/index.js";
+</script>
+
+<div class="w-6/12 mx-auto mt-16">
+    <Table.Root dir="rtl">
+        <Table.Caption>سیستم منبع باز است و هیج شخصی هیچ مسئولیتی در قبال هیچ چیزی ندارد.</Table.Caption>
+        <Table.Header dir="rtl">
+            <Table.Row>
+                <Table.Head class="text-right">نام</Table.Head>
+                <Table.Head class="text-right">نام خانوادگی</Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            <Table.Row>
+                <Table.Cell class="text-right">علی</Table.Cell>
+                <Table.Cell class="text-right">رضایی</Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+                <Table.Cell class="text-right">علی</Table.Cell>
+                <Table.Cell class="text-right">رضایی</Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+                <Table.Cell class="text-right">علی</Table.Cell>
+                <Table.Cell class="text-right">رضایی</Table.Cell>
+            </Table.Row>
+        </Table.Body>
+    </Table.Root>
+</div>
