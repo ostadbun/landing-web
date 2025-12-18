1210352ee01834266cf1bb9b4ee8964703a58fd4
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-19 00:34:05 +0330

    add and modify navbar and table

---
+++ src/components/Navbar/table/table.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    interface Accept {
+        key: string;
+        value: string;
+    }
+    let {data} = $props<{ data: Accept[] }>();
+</script>
+
+<p>This is table {data[0].key}</p>
