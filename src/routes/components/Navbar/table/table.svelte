c92274565af630bd34da917d46a2e41494c73c21
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-19 00:33:11 +0330

    add icons

--- src/routes/components/Navbar/table/table.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    interface Accept {
-        key: string;
-        value: string;
-    }
-    let {data} = $props<{ data: Accept[] }>();
-</script>
-
-<p>This is table {data[0].key}</p>
